877268b9addeab15da3bee3763eb1931811c1223
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2022-12-27 09:42:52 +0530

    Add files via upload
    onmouse over used to pop up the mage when cursor points the image in the web browser

---
+++ onmouseover.js
@@ -0,0 +1,10 @@
+//when the cursor points the image it will pop up when cursor move aways its get back to orginal size
+function changesize(){
+     document.getElementById("img").style.width="500px"; //.src="path"
+}
+
+function changesize2(){
+    document.getElementById("img").style.width="250px";  //.src="path" //250 is the original size 2nd function used to get back the image in previous size
+}
+
+//we can use the source  instead of style to change the image to another image where cursor points the image 
