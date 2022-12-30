81a06a86a4050e714df484e758c1eba7ed86e7d4
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2022-12-30 19:45:33 +0530

    Add files via upload
    Animations in java script

---
+++ fading.js
@@ -0,0 +1,9 @@
+function fadein(){
+
+
+    document.getElementById("img").style.opacity=0.3;  //opacity - made the image  transparent when fadein button clicked 
+}
+
+function fadeout(){
+    document.getElementById("img").style.opacity=0.8;  // opacity - 0.4 reduce the image fade
+}
---
+++ zoom.js
@@ -0,0 +1,6 @@
+function increase(){
+    document.getElementById("img").style.width="400px"
+}
+function decrease(){
+    document.getElementById("img").style.width="200px"
+}
